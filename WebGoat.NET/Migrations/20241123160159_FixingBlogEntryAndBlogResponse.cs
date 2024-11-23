4c91f81052670882fc329c3fe2845f60eceb4f1e
jpbe99 <Jesper.p.becker@gmail.com>
2024-11-23 17:55:44 +0100

    Blog functionality is now stable
    implemented invariens and domainprimitives for blog and it now works

---
+++ WebGoat.NET/Migrations/20241123160159_FixingBlogEntryAndBlogResponse.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Data.SqlTypes;
+using System.Xml.Linq;
+using WebGoat.NET.Models;
+
+#nullable disable
+
+namespace WebGoat.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixingBlogEntryAndBlogResponse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlogEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Content_Value = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    PostedDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+
+
+        },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogEntries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BlogResponses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BlogEntryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Content_Value = table.Column<string>(type: "TEXT", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    ResponseDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogResponses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BlogResponses_BlogEntries_BlogEntryId",
+                        column: x => x.BlogEntryId,
+                        principalTable: "BlogEntries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
