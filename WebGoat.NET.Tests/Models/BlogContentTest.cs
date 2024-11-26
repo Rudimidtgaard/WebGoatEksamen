da151d120a376d26d857beb436a9cb777716fa81
Rudimidtgaard <Rudimidtgaard@gmail.com>
2024-11-26 16:04:20 +0100

    Added first unit test

---
+++ WebGoat.NET.Tests/Models/BlogContentTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebGoat.NET.Models;
+
+namespace WebGoat.NET.Tests.Models
+{
+    public class BlogContentTest
+    {
+        [Theory]
+        [InlineData("Hello World")]
+        [InlineData("Det er en kendsgerning, at man bliver distraheret af læsbart indhold på en side, når man betragter dens layout. Meningen med at bruge Lorem Ipsum er, at teksten indeholder mere eller mindre almindelig tekstopbygning i modsætning til")]
+        public void ShouldCreateBlogContentObjectWithValidInputString(string blogContentMethodInput)
+        {
+            // Arrange
+
+            // Act
+            var actualObject = new BlogContent(blogContentMethodInput);
+
+            // Assert
+            Assert.Equal(blogContentMethodInput, actualObject.GetValue());
+        }
+    }
+}
