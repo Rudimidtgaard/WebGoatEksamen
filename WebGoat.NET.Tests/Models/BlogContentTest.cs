78d32c53e8d7433a75d38790917d8f872d94e0f6
jpbe99 <Jesper.p.becker@gmail.com>
2024-11-28 23:47:29 +0100

    updated test

--- WebGoat.NET.Tests/Models/BlogContentTest.cs
+++ WebGoat.NET.Tests/Models/BlogContentTest.cs
@@ -7,7 +7,16 @@ namespace WebGoat.NET.Tests.Models
         [Theory]
         [InlineData("HelloWorld")] // Without space
         [InlineData("Hello World")] // With space
-        [InlineData("Hello World æøå ÆØÅ")] // Danish characters
+        [InlineData("Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+         "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+         "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. " +
+         "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with " +
+          "desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. " +
+         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text " +
+         "ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+         "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. " +
+         "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software " +
+         "like Aldus PageMaker including versions of Lorem Ipsum.")] // Long Text
         [InlineData(".,!?")] // Special characters, but allowed
         public void ShouldCreateBlogContentObjectWithValidInputString(string blogContentMethodInput)
         {
@@ -39,7 +48,20 @@ namespace WebGoat.NET.Tests.Models
         }
 
         [Theory]
-        [InlineData("<Script>")]
+        [InlineData("Hello æ ø å")] // No Danish characters
+        [InlineData("<b onmouseover=\"alert(1)\">Bold Text</b>\r\n")]
+        [InlineData("<script\\n>alert(1);</script>\r\n")]
+        [InlineData("<b><script>alert(1);</b></script>\r\n")]
+        [InlineData("<script>alert(String.fromCharCode(88,83,83));</script>\r\n")]
+        [InlineData("<a href=\"javascript:alert(1)\">Click me</a>\r\n")]
+        [InlineData("<svg><script>alert(1)</script></svg>\r\n")]
+        [InlineData("<b title=\"<script>alert(1)</script>\">Bold</b>\r\n")]
+        [InlineData("<b>{{alert(1)}}</b>\r\n")]
+        [InlineData("&#x3C;&#x73;&#x63;&#x72;&#x69;&#x70;&#x74;&#x3E;alert('XSS')&#x3C;&#x2F;&#x73;&#x63;&#x72;&#x69;&#x70;&#x74;&#x3E;\r\n")]
+        [InlineData("<script>alert('XSS')</script>\r\n")]
+        [InlineData("<b style=\"background:url(javascript:alert(1))\">Bold</b>\r\n")]
+        [InlineData("&lt;scr&lt;ipt&gt;alert(1)&lt;/scr&lt;ipt&gt;\r\n")]
+        [InlineData("<p onclick=\"eval('alert(1)')\">Click me</p>\r\n")]
         [InlineData("-- //")]
         public void ShouldThrowArgumentExceptionWhenInvalidInputString(string blogContentMethodInput)
         {
